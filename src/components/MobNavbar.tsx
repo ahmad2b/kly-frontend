a1acaa2f599d020cf24c1ac408d89fbd45e75f5b
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-02-09 15:43:06 +0500

    Refactor code formatting and imports

--- src/components/MobNavbar.tsx
+++ src/components/MobNavbar.tsx
@@ -1,20 +1,24 @@
-import React from "react";
-import { AlignRight } from "lucide-react";
-import Image from "next/image";
+import { AlignRight } from 'lucide-react';
+import Image from 'next/image';
 
 function MobNavbar() {
-  return (
-    <div className="block md:hidden p-4">
-      <div className="flex justify-between">
-        <div>
-          <Image src={"/logo.svg"} alt="Logo" width={40} height={40} />
-        </div>
-        <div>
-          <AlignRight />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='block md:hidden p-4'>
+			<div className='flex justify-between'>
+				<div>
+					<Image
+						src={'/logo.svg'}
+						alt='Logo'
+						width={40}
+						height={40}
+					/>
+				</div>
+				<div>
+					<AlignRight />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default MobNavbar;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,41 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "./ui/button";
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
 
 function Navbar() {
-  return (
-    <div className="hidden md:block">
-      <div className="p-6 flex justify-between">
-        <div>
-          <Image src={"/logo.svg"} alt="Logo" width={40} height={40} />
-        </div>
-        <div className="flex gap-4">
-          <Link href="/log-in">
-            <Button
-              variant="secondary"
-              className="bg-gray-800 text-white hover:bg-gray-700"
-            >
-              Log In
-            </Button>
-          </Link>
-          
-        
-          <Link href="/sign-up">
-            <Button
-              variant="secondary"
-              className="hover:bg-gray-200"
-            >
-              Sign Up
-            </Button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='hidden md:block'>
+			<div className='p-6 flex justify-between'>
+				<div>
+					<Image
+						src={'/logo.svg'}
+						alt='Logo'
+						width={40}
+						height={40}
+					/>
+				</div>
+				<div className='flex gap-4'>
+					<Link href='/log-in'>
+						<Button
+							variant='secondary'
+							className='bg-gray-800 text-white hover:bg-gray-700'
+						>
+							Log In
+						</Button>
+					</Link>
+
+					<Link href='/sign-up'>
+						<Button
+							variant='secondary'
+							className='hover:bg-gray-200'
+						>
+							Sign Up
+						</Button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
